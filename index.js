f16f9dd510f8669425e731b1450a530f3867524b
Victor John Makanda <victormakanda254@gmail.com>
2023-09-15 18:24:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+//const newHeader= document.getElementById('h1');
+
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent="Victor is the champion";
+document.body.append(newHeader);
+
+document.querySelector('main#main').remove();
+
+
+
